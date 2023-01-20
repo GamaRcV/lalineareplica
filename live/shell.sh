8e93318d073a1ed3906da543ae60a0bf127ee937
GamaRcV <110438108+GamaRcV@users.noreply.github.com>
2023-01-20 16:38:30 -0600

    eliminando codigo .sh

--- live/shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "$(date +"%I:%M %p")" > hora.txt
-VIDSOURCE="rtsp://admin:admin123456@server-tpd.dyndns-ip.com:1500"
-AUDIO_OPTS="-c:a aac -b:a 0 -ac 2"
-VIDEO_OPTS="  -s 1280x720 -c:v libx264 -b:v 80000"
-OUTPUT_HLS=" -hls_time 1 -hls_list_size 2 -start_number 1 -hls_flags delete_seg$
-ffmpeg -i "$VIDSOURCE" -y -t 20   $AUDIO_OPTS $VIDEO_OPTS  $OUTPUT_HLS  mystrea$
